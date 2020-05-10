c15fd9a88d62c586ad4c4b70f22fda9d855c7165
Ferdinando <ferdi.dagostino@gmail.com>
2020-05-10 13:40:12 +0200

    in lavorazione

---
+++ main.js
@@ -0,0 +1,18 @@
+//
+// $('div.contacts').click(function() {
+//     var currentContact = $('.contacts.active');
+//     console.log( $('.contacts.active'));
+//     var nextContact = currentContact.next('.contacts');
+//     if (currentContact.hasClass('active')) {
+//         currentContact.removeClass('active');
+//         nextContact.addClass('active');
+//     }
+//
+// });
+
+
+
+$('div.contacts').click(function() {
+
+
+});
