af94c4953e9a9776db5ac397d21400ec181e9f4c
Ferdinando <ferdi.dagostino@gmail.com>
2020-05-11 13:37:49 +0200

    milestone 1 completata, da costruire il template

--- main.js
+++ main.js
@@ -17,18 +17,24 @@ $('div.contacts').click(function() {
 
 });
 
-//per associare il tasto enter all'input
+//per associare  la pressione del tasto enter nella textarea al bottone
 $("#input-text").keydown(function(event) {
     if (event.keyCode === 13) {
-        $('#input-text').click();
+        $('#input-button').click();
 
     }
+
+
 });
 
-$("#input-text").click(function() {
-
+$("#input-button").click(function() {
+    //prendo ciò che è stato scritto dall'utente nell'input e lo salvo in una variabile
     var messaggio = $('#input-text').val();
     console.log(messaggio);
-    $('.reply').append("<p>" + messaggio + "</p>")
+    //se il messaggio non è vuoto, allora può essere speidto
+    if (messaggio != '') {
+        $('.reply').append("<p class=risposta>" + messaggio + "</p>")
+        $('#input-text').val('');
+    }
 
 });
