23245f0255235540384060ffc6c67a1311f0d72e
Ferdinando <ferdi.dagostino@gmail.com>
2020-05-11 12:35:59 +0200

    da costruire il template

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ $('div.contacts').click(function() {
 
 
 });
+
+//per associare il tasto enter all'input
+$("#input-text").keydown(function(event) {
+    if (event.keyCode === 13) {
+        $('#input-text').click();
+
+    }
+});
+
+$("#input-text").click(function() {
+
+    var messaggio = $('#input-text').val();
+    console.log(messaggio);
+    $('.reply').append("<p>" + messaggio + "</p>")
+
+});
